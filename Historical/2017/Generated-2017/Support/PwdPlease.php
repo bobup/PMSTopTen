87b00305cb8974923fe10142e6fa2d3f7c4c5bcb
Bob Upshaw <bobup@acm.org>
2018-07-05 10:06:34 -0700

    Changed the name of some directories.  Fixed typo in DoFetchAndProcessTopten

---
+++ Historical/2017/Generated-2017/Support/PwdPlease.php
@@ -0,0 +1,32 @@
+<?php
+/* 
+ * PwdPlease.php - display the contents of the parent directory of the
+ *	directory holding this script.
+ */
+ 
+ 
+$files = scandir( ".." );
+$cwdName = getcwd();
+$dirSimpleName = basename( dirname( $cwdName ) );
+?>
+<html>
+<head>
+<title><?php
+echo "Contents of $dirSimpleName";
+?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+<?php
+echo "<h2>Contents of $dirSimpleName</h2>\n";
+
+foreach( $files as $file ) {
+	if( substr( $file, 0, 1 ) === '.' ) continue;
+	$dir = "";
+	if( is_dir( $file ) ) $dir = '/';
+	echo "<br>$file$dir\n";
+}
+
+?>
+</body>
+</html>
